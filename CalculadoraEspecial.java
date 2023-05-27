bf8fe05334532a389e5123d78fcbbee80fb9a23a
kimberly villeda <kvilledar1@miumg.com.edu>
2023-05-26 19:13:00 -0600

    Subiendo Poo

---
+++ CalculadoraEspecial.java
@@ -0,0 +1,43 @@
+package calculadora.poo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Random;
+/**
+ *
+ * @author Alfredo Villeda
+ */
+public class CalculadoraEspecial {
+  public int generarNumeroAleatorio(int limiteSuperior) {
+        Random random = new Random();
+        return random.nextInt(limiteSuperior);
+    }
+
+    public double calcularPotencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+    
+    public double calcularLogaritmo(double numero) {
+        return Math.log(numero);
+    }
+
+    public double calcularSeno(double anguloEnRadianes) {
+        return Math.sin(anguloEnRadianes);
+    }
+
+    public double calcularCoseno(double anguloEnRadianes) {
+        return Math.cos(anguloEnRadianes);
+    }
+    
+    public double calcularTangente(double anguloEnRadianes) {
+        return Math.tan(anguloEnRadianes);
+    }
+    
+    public double calcularRaizCubica(double numero) {
+        return Math.cbrt(numero);
+    }
+    
+   
+}
